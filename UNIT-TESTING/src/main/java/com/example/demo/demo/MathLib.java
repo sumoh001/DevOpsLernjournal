c0aed6d683161e2144ab92f7cc75d5aa52ddc3eb
sumoh001 <umoh.simon@yahoo.de>
2024-05-26 15:07:26 +0200

    -

---
+++ UNIT-TESTING/src/main/java/com/example/demo/demo/MathLib.java
@@ -0,0 +1,11 @@
+package com.example.demo.demo;
+
+@SpringBootApplication
+public class MathLib {
+
+	public static boolean isEven(int value) {
+	return value % 2 == 0;
+	}
+   
+}
+
---
+++ UNIT-TESTING/src/test/java/com/example/demo/demo/MathLibTest.java
@@ -0,0 +1,12 @@
+package com.example.demo.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MathLibTest {
+ @Test
+ public void testEven1() {
+ assertTrue(MathLib.isEven(2));
+ }
+}
